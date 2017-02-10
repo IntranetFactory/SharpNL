1a86acb4810806f2039a3c52e9181d96d0ba55d1
Ivan Jovanovic <ij.smartnet@gmail.com>
2017-02-10 16:30:07 +0100

    -- enabled inline id parsing

--- src/ModelGeneratorW/Parsers/InlineIdIntentParser.cs
+++ src/ModelGeneratorW/Parsers/InlineIdIntentParser.cs
@@ -30,107 +30,107 @@ namespace ModelGeneratorW.Parsers
 
             Dictionary<string, IntentsOutputObject> currentData = new Dictionary<string, IntentsOutputObject>();
 
-            //for (int fileInfoIndex = 0; fileInfoIndex < intentFileInfos.Count; fileInfoIndex++)
-            //{
-            //    // intentFileInfo has info about intent definitions 
-            //    // as content to be processed intentFileInfo has list of lines read from file
-            //    var intentFileInfo = intentFileInfos[fileInfoIndex];
+            for (int fileInfoIndex = 0; fileInfoIndex < intentFileInfos.Count; fileInfoIndex++)
+            {
+                // intentFileInfo has info about intent definitions 
+                // as content to be processed intentFileInfo has list of lines read from file
+                var intentFileInfo = intentFileInfos[fileInfoIndex];
 
-            //    int defaultLineIndex = ignoreHeaderLines ? 1 : 0;
+                int defaultLineIndex = ignoreHeaderLines ? 1 : 0;
 
-            //    for (int lineIndex = defaultLineIndex; lineIndex < intentFileInfo.Lines.Count; lineIndex++)
-            //    {
-            //        var line = intentFileInfo.Lines[lineIndex];
+                for (int lineIndex = defaultLineIndex; lineIndex < intentFileInfo.Lines.Count; lineIndex++)
+                {
+                    var line = intentFileInfo.Lines[lineIndex];
 
-            //        var lineParts = this.intentLineSplitter.Split(line);
-            //        if (lineParts.Length < 2) { continue; }
+                    var lineParts = this.intentLineSplitter.Split(line);
+                    if (lineParts.Length < 2) { continue; }
 
-            //        var intentId = lineParts[0].Trim();
-            //        var intentText = lineParts[1];
+                    var intentId = lineParts[0].Trim();
+                    var intentText = lineParts[1];
 
-            //        if (string.IsNullOrEmpty(intentId)) { continue; }
-            //        if (string.IsNullOrEmpty(intentText)) { continue; }
+                    if (string.IsNullOrEmpty(intentId)) { continue; }
+                    if (string.IsNullOrEmpty(intentText)) { continue; }
 
-            //        var intentsOutputObject = new IntentsOutputObject();
-            //        var intentResponse = new IntentResponse();
+                    var intentsOutputObject = new IntentsOutputObject();
+                    var intentResponse = new IntentResponse();
 
-            //        if (currentData.ContainsKey(intentId))
-            //        {
-            //            intentsOutputObject = currentData[intentId];
-            //            intentResponse = intentsOutputObject.Responses[0];
-            //        }
-            //        else
-            //        {
-            //            intentsOutputObject.Id = Guid.NewGuid().ToString();
-            //            intentsOutputObject.Name = intentId;
+                    if (currentData.ContainsKey(intentId))
+                    {
+                        intentsOutputObject = currentData[intentId];
+                        intentResponse = intentsOutputObject.Responses[0];
+                    }
+                    else
+                    {
+                        intentsOutputObject.Id = Guid.NewGuid().ToString();
+                        intentsOutputObject.Name = intentId;
 
-            //            intentsOutputObject.UserSays = new List<UserSay>();
+                        intentsOutputObject.UserSays = new List<UserSay>();
 
-            //            intentResponse = new IntentResponse();
-            //            intentsOutputObject.Responses.Add(intentResponse);
-            //            currentData.Add(intentId, intentsOutputObject);
-            //        }
+                        intentResponse = new IntentResponse();
+                        intentsOutputObject.Responses.Add(intentResponse);
+                        currentData.Add(intentId, intentsOutputObject);
+                    }
 
-            //        UserSay userSay = new UserSay();
-            //        userSay.Id = Guid.NewGuid().ToString();
-            //        userSay.Data = new List<UserSayData>();
+                    UserSay userSay = new UserSay();
+                    userSay.Id = Guid.NewGuid().ToString();
+                    userSay.Data = new List<UserSayData>();
 
-            //        var intentLineParserResult = this.intentLineParser.Parse(intentText);
-            //        if (intentLineParserResult.HasError)
-            //        {
-            //            Console.WriteLine("Ignoring line {0} of file {1}: {2}", lineIndex + 1, intentFileInfo.IntentName, line);
-            //            Console.WriteLine("Error {0}", intentLineParserResult.ErrorMessage);
+                    var intentLineParserResult = this.intentLineParser.Parse(intentText);
+                    if (intentLineParserResult.HasError)
+                    {
+                        Console.WriteLine("Ignoring line {0} of file {1}: {2}", lineIndex + 1, intentFileInfo.IntentName, line);
+                        Console.WriteLine("Error {0}", intentLineParserResult.ErrorMessage);
 
-            //            if (intentsOutputObject.UserSays.Count == 0)
-            //            {
-            //                currentData.Remove(intentId);
-            //            }
+                        if (intentsOutputObject.UserSays.Count == 0)
+                        {
+                            currentData.Remove(intentId);
+                        }
 
-            //            continue;
-            //        }
-            //        else
-            //        {
-            //            // if any entities were found, see if they already exist in entities list
-            //            // if not add new entities to already existing entities list
-            //            for (int entityIndex = 0; entityIndex < intentLineParserResult.Entities.Count; entityIndex++)
-            //            {
-            //                Entity entity = intentLineParserResult.Entities[entityIndex];
+                        continue;
+                    }
+                    else
+                    {
+                        // if any entities were found, see if they already exist in entities list
+                        // if not add new entities to already existing entities list
+                        for (int entityIndex = 0; entityIndex < intentLineParserResult.Entities.Count; entityIndex++)
+                        {
+                            Entity entity = intentLineParserResult.Entities[entityIndex];
 
-            //                // if recognized entity doesn't exist add it to list of entities
-            //                var existingEntity = entities.FirstOrDefault(ent => ent.EntityName == entity.EntityName);
-            //                if (existingEntity == null)
-            //                {
-            //                    Console.WriteLine("Entity {0} not present in list of entities", entity.EntityName);
-            //                    entities.Add(entity);
-            //                    Console.WriteLine("Entity {0} added to list of entities", entity.EntityName);
-            //                }
-            //                else
-            //                {
-            //                    List<string> synonymsToConcat = new List<string>();
-            //                    foreach (var synonym in entity.Synonyms)
-            //                    {
-            //                        if (!existingEntity.Synonyms.Any(s => s == synonym))
-            //                        {
-            //                            synonymsToConcat.Add(synonym);
-            //                        }
-            //                    }
-            //                    existingEntity.Synonyms = existingEntity.Synonyms.Concat(synonymsToConcat).ToArray();
-            //                }
+                            // if recognized entity doesn't exist add it to list of entities
+                            var existingEntity = entities.FirstOrDefault(ent => ent.EntityName == entity.EntityName);
+                            if (existingEntity == null)
+                            {
+                                Console.WriteLine("Entity {0} not present in list of entities", entity.EntityName);
+                                entities.Add(entity);
+                                Console.WriteLine("Entity {0} added to list of entities", entity.EntityName);
+                            }
+                            else
+                            {
+                                List<string> synonymsToConcat = new List<string>();
+                                foreach (var synonym in entity.Synonyms)
+                                {
+                                    if (!existingEntity.Synonyms.Any(s => s == synonym))
+                                    {
+                                        synonymsToConcat.Add(synonym);
+                                    }
+                                }
+                                existingEntity.Synonyms = existingEntity.Synonyms.Concat(synonymsToConcat).ToArray();
+                            }
 
-            //                if (!intentResponse.Parameters.Any(p => p.Name == entity.EntityName))
-            //                {
-            //                    IntentParameter parameter = new IntentParameter(entity.EntityName);
-            //                    intentResponse.Parameters.Add(parameter);
-            //                }
-            //            }
+                            if (!intentResponse.Parameters.Any(p => p.Name == entity.EntityName))
+                            {
+                                IntentParameter parameter = new IntentParameter(entity.EntityName);
+                                intentResponse.Parameters.Add(parameter);
+                            }
+                        }
 
-            //            // add UserSayData objects to userSay output object
-            //            userSay.Data.AddRange(intentLineParserResult.UserSayDatas);
-            //        }
+                        // add UserSayData objects to userSay output object
+                        userSay.Data.AddRange(intentLineParserResult.UserSayDatas);
+                    }
 
-            //        intentsOutputObject.UserSays.Add(userSay);
-            //    }
-            //}
+                    intentsOutputObject.UserSays.Add(userSay);
+                }
+            }
 
             result = currentData.Values.ToList();
 
